c60642eece0ee4fc0f5b0f019f4425312c399c4c
LuWo0 <loviedo46@yahoo.com>
2022-07-10 03:04:09 -0700

    Added responsiveness for small screens

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,24 +1,84 @@
 const gridContainer = document.querySelector(".grid-container");
 const cell = document.querySelector(".cell");
+const smallScreenFit = document.querySelector(":root");
 
-function createGrid(rows, cols){
+
+function createGrid(rows, cols, width, height){
+    removeAllChildNodes(gridContainer);
     gridContainer.style.setProperty("--grid-rows", rows);
     gridContainer.style.setProperty("--grid-cols", cols);
-
-    for (let i = 0; i < (rows * cols); i++){
+    gridContainer.style.setProperty("--cell-width", width);
+    gridContainer.style.setProperty("--cell-height", height);
+    
+    for (let i = 0; i < (rows * cols); i++){ // creates a square grid with equal cells
         const cell = document.createElement("div");
         cell.classList.add("cell");
-        // cell.innerText = (i + 1);
         gridContainer.appendChild(cell);
         fillCell(cell, "red");
     }
 }
 
-function fillCell(cell, color) {
+function fillCell(cell, color) { //Fills the cell with a specified color
     cell.addEventListener("mouseover", () => {
         cell.setAttribute("style", `background-color: ${color};`)
     });
 }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+// function makeGridSmall(){
+//     const smallGrid = document.querySelector(".small-grid");
+//     smallGrid.addEventListener("click", () => {
+//         const newCellWidth = document.querySelector(":root");
+//         const newCellHeight = document.querySelector(":root");
+//         const newRowNum = document.querySelector(":root");
+//         const newColNum = document.querySelector(":root");
 
-createGrid(16, 16);
+//         newCellWidth.style.setProperty("--cell-width", "48px");
+//         newCellHeight.style.setProperty("--cell-height", "48px");
+//         newRowNum.style.setProperty("--grid-rows", "16");
+//         newColNum.style.setProperty("--grid-cols", "16");
+//     });
+// }
+function main(){
+    let rows = 32, cols = 32, width = "16px", height = "16px"; //setting the grid to default medium values
+    createGrid(rows, cols, width, height); 
+
+    const small = document.querySelector(".small-grid");
+    small.addEventListener("click", () => {
+        rows = 16;
+        cols = 16;
+        width = "32px";
+        height = "32px";
+        createGrid(rows, cols, width, height);
+        smallScreenFit.style.setProperty("--adjust-value", "8");
+        
+    });
+
+    const medium = document.querySelector(".medium-grid");
+    medium.addEventListener("click", () => {
+        rows = 32;
+        cols = 32;
+        width = "16px";
+        height = "16px";
+        createGrid(rows, cols, width, height);
+        smallScreenFit.style.setProperty("--adjust-value", "16");
+    });
+
+    const large = document.querySelector(".large-grid");
+    large.addEventListener("click", () => {
+        rows = 64;
+        cols = 64;
+        width = "8px";
+        height = "8px";
+        createGrid(rows, cols, width, height);
+        smallScreenFit.style.setProperty("--adjust-value", "32");
+    
+    });
+}
+
+main();
+
